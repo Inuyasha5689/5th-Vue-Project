67de4041055c0f4564a738e8bb58bb6826251d11
Drake Scott <dascott0204@gmail.com>
2017-02-02 17:10:56 -0600

    Set styles dynamically without CSS classes and using the array syntax.

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ new Vue ({
     el: '#app',
     data: {
         attachRed: false,
-        color: 'green'
+        color: 'green',
+        color2: 'gray',
+        width: 100
     },
     computed: {
         divClasses: function() {
             return {
                 red: this.attachRed,
                 blue: !this.attachRed
+            };
+        },
+        myStyle: function() {
+            return {
+                backgroundColor: this.color2,
+                width: this.width + 'px'
             };
         }
     }
